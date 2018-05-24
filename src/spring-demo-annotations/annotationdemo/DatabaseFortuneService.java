88ad67deee5b35ec239d06d9c87e1d673d4e4997
Alexander Smotrikov <alexandersmotrikov@gmail.com>
2018-05-24 15:27:09 +0300

    add services for @Qualifier annotation, now with beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'annotationdemo.FortuneService' available: expected single matching bean but found 4: Error

---
+++ src/spring-demo-annotations/annotationdemo/DatabaseFortuneService.java
@@ -0,0 +1,11 @@
+package annotationdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
