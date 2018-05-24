01a4faee07f6ba99b4ecf2ed1f252e8f6cefddb3
Alexander Smotrikov <alexandersmotrikov@gmail.com>
2018-05-24 16:30:08 +0300

    Add JavaSpringConfiguration from Java Class

---
+++ src/main/javaspringconfiguration/SportConfig.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("annotationdemo")
+public class SportConfig {
+
+
+}
+
